a917fee15bf2918f23f343f7d1b0848cdcf9707c
Valentin_G <valentin.guillon93430@gmail.com>
2022-11-21 15:01:53 +0100

    mis des couleurs dans les background, renommé des trucs (train dans Train -> wagons) (dans Train, dans le y de la boucle, NB_BANDITS -> NB_WAGONS) suppresion class Marshall

--- colt_express_ze3ma.py
+++ colt_express_ze3ma.py
@@ -12,23 +12,26 @@ class Game(Tk):
     def __init__(self):
         super().__init__()
         self.train = Train(self, 500, 750)
+        self.train["bg"] = "orange"
         self.train.grid(row = 0, column = 0, sticky =  'nsew')
-        self.columnconfigure(0, weight = 3) 
+        self.columnconfigure(0, weight = 3)
         self.columnconfigure(1, weight = 1)
         self.rowconfigure(0, weight=3)
-        self.rowconfigure(1, weight=1) 
-        self.title("Colt Express ")
+        self.rowconfigure(1, weight=1)
+        self.title("Colt Express")
         self.geometry("720x480")
-        self.iconbitmap("./train.ico")
+        # self.iconbitmap("./train.ico")
         for i in range(NB_JOUEURS):
+            print(i)
             self.train.rowconfigure(i, weight=1)
             self.train.columnconfigure(i, weight=1)
         self.bandits = []
-    # Pour le Marshall     
+
+
+    # Pour le Marshall
     def deplecement(self):
-        
         #! Cette fonction retourna une position aleatoire qui sera passé dans Train/ Wagon , pour verifié si le marshall y est ( Marshall = 1/True )
-        
+
         #on verifie si le Marshall est en tete ou s*queue de train 
         #si pos == 0
             #Deplacer a droite
@@ -36,7 +39,7 @@ class Game(Tk):
         #si pos == train[wagons-1]
             #Deplacer a gauche 
             #return
-         
+
         if (random.randint(0,1)):
             
             #deplacement a gauche
@@ -44,44 +47,31 @@ class Game(Tk):
         else:
             #deplacement a droite
             pass
+
+
+
 class Train(Canvas):
-    train = []
-    #Constructeur 
-    def __init__(self,fenetre : Tk, width, height):
-        super().__init__(fenetre, width= width, height =height, bg='ivory')
+    wagons = []
+    #Constructeur
+    def __init__(self, fenetre :Tk, width, height):
+        super().__init__(fenetre, width=width, height=height, bg='green')
         #Créer les cases
-        for x in range(0, 2 ):
-            for y in range(0, NB_JOUEURS):
-                self.train.append(Wagon(self, x, y))
-    pass
+        for x in range(2):
+            for y in range(NB_WAGONS):
+                self.wagons.append(Wagon(self, x, y))
 
 
-""" class Marshall():
-    def __init__(self, position):
-        self.position = position
-        
-    
-    
-    def deplecement(self):
-        
-        #on verifie si le Marshall est en tete ou s*queue de train 
-        #si pos == 0
-            #Deplacer a droite
-            #return
-        #si pos == train[wagons-1]
-            #Deplacer a gauche 
-            #return
-         
-        if (random.randint(0,1)):
-            
-            #deplacement a gauche
-            pass
-        else:
-            #deplacement a droite
-            pass
-        """
-        
-        
+
+
+class Wagon(Canvas):
+    def __init__(self, Train:Canvas, x , y):
+        super().__init__(Train, width=100, height=100, highlightbackground='red')
+        self.x = x
+        self.y = y
+        self.marshall = False
+        self.grid(row=x, column=y, sticky= 'nsew')
+
+
 
 class Bandit(Canvas):
     def __init__(self, name):
@@ -89,25 +79,21 @@ class Bandit(Canvas):
         self.position = ()
         self.actions = []
         self.marshall = 0
-        
-    
-    
+
+
+
     def deplecement(self):
-        
         pass
-        
-        
-        
-class Wagon(Canvas):
-    def __init__(self,Train:Canvas, x , y):
-        super().__init__(Train, width=100, height=100, highlightbackground='ivory')
-        self.x = x
-        self.y = y
-        self.marshall = False
-        self.grid(row=x, column=y, sticky= 'nsew')       
-        
+
+
+
+
+
 mon_jeu = Game()
 
 
-mon_jeu.mainloop()
+for wagon in mon_jeu.train.wagons:
+    print (wagon.marshall)
 
+
+mon_jeu.mainloop()
